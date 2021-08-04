7857fb9050290e1a5a3fc6cc401befcaf78bd02b
adigi-1775 <adigi1775@gmail.com>
2021-08-03 20:23:27 -0400

    Created buttons html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log('Tamagotchi');
+
+// const name = []
+//
+// class Tamagotchi {
+//   constructor(age, hunger, sleepiness, boredom){
+//   this.age = age
+//   this.hunger = 1 - 10
+//   this.sleepiness = 1 - 10
+//   this.boredom = 1 - 10
+//   }
+// }
+// feedTama(){
+//
+// }
+// playWithTama(){
+//
+// }
+// tamaSleep(){
+//
+// }
