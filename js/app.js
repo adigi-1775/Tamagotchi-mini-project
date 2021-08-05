dcb3bde9cfb92027e0ef139474ea284244fdb01e
adigi-1775 <adigi1775@gmail.com>
2021-08-04 21:23:52 -0400

    created submit event listener

--- js/app.js
+++ js/app.js
@@ -1,21 +1,161 @@
-console.log('Tamagotchi');
+// console.log('Tamagotchi');
 
-// const name = []
+const name = []
+// alert('To begin, give your Tamagotchi a name!')
+
+class Tamagotchi {
+  constructor(name, age){
+  this.name = name
+  this.age = 0
+  this.hunger = 0
+  this.sleepiness = 0
+  this.boredom = 0
+  this.dead = false
+}
+feedTama(){
+  this.hunger--
+  let feedTime = createElement('div')
+      feedTime.setAttribute('id', 'feed-tamagotchi')
+
+  let feedTime1 = document.querySelector('#feed-tamagotchi')
+}
+playWithTama(){
+  this.boredom--
+
+}
+tamaSleep(){
+  this.sleepiness--
+
+}
+increaseAge(){
+  this.age++
+
+}
+increaseHunger(){
+  this.hunger++
+
+}
+increaseSleepiness(){
+  this.sleepiness++
+
+}
+increaseBoredom(){
+  this.boredom++
+
+}
+// death(){
+
+// }
+// morph(){
+
+// {
+
+}
+
+// game logic
+// const game = {
+//   time: 0,
+//   startTimer(){
+//     this.intervalId = setInterval(()=>{
+//       this.time++
+//       const timeInMillisecond = this.time * 1000
 //
-// class Tamagotchi {
-//   constructor(age, hunger, sleepiness, boredom){
-//   this.age = age
-//   this.hunger = 1 - 10
-//   this.sleepiness = 1 - 10
-//   this.boredom = 1 - 10
+//
+//       let timerElement = document.querySelector('#time')
+//           timerElement.innerHTML = ()
+//
+//     })
+//   },
+//
+//
+//   start(name){
+//     const myTama1 = new Tamagotchi(name)
+//     this.myTama = myTama1
 //   }
-// }
-// feedTama(){
 //
 // }
-// playWithTama(){
+
+
+
+// event listeners
+let form = document.getElementById('name-button')
+    form.addEventListener('click', (event)=>{
+      event.preventDefault();
+let submitName = document.getElementById('add-name').value;
+    document.querySelector('h1').innerHTML = `${submitName}`
+})
+
+let button = document.getElementById('Feed')
+    button.addEventListener('click', (event)=>{
+      event.preventDefault();
+let feeding = document.getElementById('hunger').value;
+    document.querySelector('.buttons').innerHTML = `${feeding}`
+})
+
+let button = document.getElementById('Sleep')
+    button.addEventListener('click', (event)=>{
+      event.preventDefault();
+let feeding = document.getElementById('hunger').value;
+    document.querySelector('.buttons').innerHTML = `${feeding}`
+})
+
+let button = document.getElementById('Play')
+    button.addEventListener('click', (event)=>{
+      event.preventDefault();
+let feeding = document.getElementById('hunger').value;
+    document.querySelector('.buttons').innerHTML = `${feeding}`
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
-// }
-// tamaSleep(){
-//
-// }
